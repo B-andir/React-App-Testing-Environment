769192555626cb77afd16c29539652ed2954051c
B-andir <svenberg.daniel@gmail.com>
2021-06-26 22:31:38 +0200

    Simple manipulation of UI - no back-end

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,34 @@
-const Header = () => {
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({ title }) => {
+
+    const onClick = () => {
+        console.log('Click')
+    }
+
     return (
-        <header>
-            <h1>Task Tracker</h1>
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color='green' text='Hello' onClick={onClick} />
         </header>
     )
 }
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string,
+}
+
+
+// Example of CSS in JS. Would be passed in as an element in the tag where to be applied. style={headingStyle}
+
+// const headingStyle = {
+//     color: 'red',
+//     backgroundColor: 'black',
+// }
 
 export default Header
